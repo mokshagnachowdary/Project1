0c9db434cfcfa172f009183d9f38379d352d8780
mokshagnachowdary <mokshagnachowdharyy@gmail.com>
2026-01-03 10:11:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Welcome to FEDFUI Class</h1>
+      <h2>2500080020 - Mokshagna Chowdary</h2>
+      <h3>3.1.26</h3>
+    </div>
+  )
+}
+
+export default App
